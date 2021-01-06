898b5695dd3e9eea910ca9f61dff790fef1cfc24
LABCAT <shane.watters.nz@gmail.com>
2021-01-07 12:32:15 +1300

    Finalising sketch.

--- src/js/P5Sketch.js
+++ src/js/P5Sketch.js
@@ -1,43 +1,37 @@
 import React, { useEffect } from "react";
+import PlayIcon from './PlayIcon.js';
 import './globals';
 import "p5/lib/addons/p5.sound";
 import * as p5 from "p5";
 import audio from '../audio/strings-no-1.mp3'
-//import audio from '../audio/donuts-no-1.mp3'
 
 
 const P5Sketch = () => {
 
     const Sketch = p5 => {
 
-        p5.canvasWidth = window.innerWidth;
-
-        p5.canvasHeight = window.innerHeight;
-
         p5.canvas = null;
+        p5.canvasWidth = window.innerWidth;
+        p5.canvasHeight = window.innerHeight;
         
         p5.song = null;
-
+        p5.creditsLogged = false;
         p5.tempo = 106;
 
-        p5.nextSemiQuaver = 5;
-
         p5.melodyIndex = 0;
-
-        p5.melodyArray = [5,3,4,4,5,3,4,4];
+        p5.melodyArray = [5,3,4,4,5,3,4,4,5,3,4,2,2,3,1,3,1,3,1];
+        p5.nextSemiQuaver = 5;
+        p5.semiQuaversArray = [];
 
         p5.numOfStringLoops = 300;
-
         p5.nx = p5.random(100);
         p5.ny = p5.random(100);
         p5.nz = 0;
-
         p5.h = p5.random(360);
-
         p5.ox = p5.random(p5.canvasWidth);
         p5.oy = p5.random(p5.canvasHeight);
         
-        p5.semiQuaversArray = [];
+        
 
 
         p5.setup = () => {
@@ -49,22 +43,30 @@ const P5Sketch = () => {
             p5.noFill();
         };
 
-        //https://www.openprocessing.org/sketch/988880
         p5.draw = () => {
             let currentBar = p5.getSongBar();
             
             p5.blendMode(p5.SCREEN);
 
-            //console.log('currentBar', currentBar);
-            if (p5.song._lastPos > 0 && currentBar >= 0) {
+            if (p5.song._lastPos > 0 && currentBar >= 0 && p5.song.isPlaying()) {
 
-                if (currentBar > 95) {
-                    currentBar = 95;
+                if (currentBar > 209) {
+                    currentBar = 209;
                     p5.canvas.addClass('fade-out');
+                    p5.canvas.removeClass('fade-in');
+
+                    if (!p5.creditsLogged){
+                        p5.creditsLogged = true;
+                        console.log('Music By: https://github.com/LABCAT');
+                        console.log('Animation By: https://github.com/LABCAT');
+                        console.log('Code Inspiration: https://www.openprocessing.org/sketch/988880');
+                    }
+                }
+                else {
+                    p5.canvas.addClass('fade-in');  
+                    p5.canvas.removeClass('fade-out');
                 }
                 p5.playStrings();
-                console.log('currentBar');
-                console.log(currentBar);
                 if (currentBar === p5.nextSemiQuaver) {
                     p5.melodyIndex++;
                     if (p5.melodyIndex === p5.melodyArray.length){
@@ -86,36 +88,39 @@ const P5Sketch = () => {
         };
 
         p5.playStrings = () => {
-            console.log('newStrings');
             p5.stroke(p5.h % 360, 100, 100, 50);
 
             p5.beginShape();
             const numOfLoops = p5.numOfStringLoops;
-            //const numOfLoops = p5.random(400, 800);
             
-            const circleSize = p5.canvasHeight / 256;
             for (let i = 0; i < numOfLoops; i++) {
                 let x = p5.map(p5.noise(i * 0.01, p5.nx, p5.nz), 0, 1, p5.ox - numOfLoops, p5.ox + numOfLoops);
                 let y = p5.map(p5.noise(i * 0.01, p5.ny, p5.nz), 0, 1, p5.oy - numOfLoops, p5.oy + numOfLoops);
                 p5.curveVertex(x, y);
-                //p5.ellipse(x, y, circleSize, circleSize);
             }
 
             p5.endShape();
-            console.log('ednShape');
 
             p5.nz += 0.005;
-            
+        }
 
-            // if (p5.nz > 0.5) {
-            //     p5.nz = 0;
-            //     p5.nx = p5.random(100);
-            //     p5.ny = p5.random(100);
-            //     p5.ox = p5.random(p5.canvasWidth);
-            //     p5.oy = p5.random(p5.canvasHeight);
-            //     p5.h = p5.random(360);
-            // }
-            // p5.h++;
+        p5.reset = () => {
+            //variables
+            p5.nextSemiQuaver = 5;
+            p5.melodyIndex = 0;
+            p5.numOfStringLoops = 300;
+            p5.nx = p5.random(100);
+            p5.ny = p5.random(100);
+            p5.nz = 0;
+            p5.h = p5.random(360);
+            p5.ox = p5.random(p5.canvasWidth);
+            p5.oy = p5.random(p5.canvasHeight);
+            //reset canvas to initial state
+            p5.clear();
+            p5.background(0);
+            //play song again
+            p5.song.stop();
+            p5.song.play();
         }
 
         p5.getSongBar = () => {
@@ -132,7 +137,13 @@ const P5Sketch = () => {
             if (p5.song.isPlaying()) {
                 p5.song.pause();
             } else {
-                p5.song.play();
+                document.getElementById("play-icon").classList.add("fade-out");
+                if (p5.getSongBar() > 210){
+                    p5.reset();
+                }
+                else {
+                    p5.song.play();
+                }
             }
         };
 
@@ -163,15 +174,23 @@ const P5Sketch = () => {
         else {
             //The browser does not support Javascript event binding
         }
+
+        p5.getSong = () => {
+            return p5.song;
+        }
     };
+
 
     useEffect(() => {
         new p5(Sketch);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+
     return (
-        <></>
+        <>
+            <PlayIcon />
+        </>
     );
 };
 
